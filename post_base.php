5686d5ff675a8ca6d5f4ac9b677ebe0629267e6e
ArnoldClarke <aclarke@mines.edu>
2013-11-12 21:07:59 -0700

    Fixed comments

--- post_base.php
+++ post_base.php
@@ -7,19 +7,19 @@
 
 <?php startblock('content') ?>
 	
-	//Here be the title of the post
+	<!-- Here be the title of the post -->
 	<h3 id ='title-request'> </h3>
 
-	//Here be the language of the post
+	<!-- Here be the language of the post -->
 	<div id='language-request'><p> </p></div>
 	
 	
-	//Here be the description of the post
+	<!-- Here be the description of the post -->
 	<div id = 'description-request'> <p> </p></div>
 	
 	
-	//Here be the list of replies
+	<!-- Here be the list of replies -->
 	
-	//Here be comments
+	<!-- Here be comments -->
 	
 <?php endblock() ?>
