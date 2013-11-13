dc64061a652251e5e08f101e49ea49694f59f06d
Vander Rice <vanxrice@gmail.com>
2013-11-12 19:08:05 -0700

    Removing uncessary backticks

--- create_database.sql
+++ create_database.sql
@@ -4,16 +4,16 @@ USE team06;
 -- user submitted requests for code or algorithm
 -- ---
 
-DROP TABLE IF EXISTS `request`;
+DROP TABLE IF EXISTS request;
     
-CREATE TABLE `request` (
-  `id` PRIMARY KEY INT NULL AUTO_INCREMENT,
-  `owner_id` INT NOT NULL,
-  `accepted_submission_id` INT NULL,
-  `title` VARCHAR(1024) NOT NULL DEFAULT 'NULL',
-  `description` MEDIUMTEXT NULL,
-  `price_min` DECIMAL NULL,
-  `price_max` DECIMAL NULL,
+CREATE TABLE request (
+  id PRIMARY KEY INT NULL AUTO_INCREMENT,
+  owner_id INT NOT NULL,
+  accepted_submission_id INT NULL,
+  title VARCHAR(1024) NOT NULL DEFAULT 'NULL',
+  description MEDIUMTEXT NULL,
+  price_min DECIMAL NULL,
+  price_max DECIMAL NULL,
   FOREIGN KEY(owner_id) REFERENCES user(id),
   FOREIGN KEY(accepted_submission_id) REFERENCES submission(id)
 );
@@ -23,15 +23,15 @@ CREATE TABLE `request` (
 -- we don't encrypt passwords, plaintext ftw
 -- ---
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS user;
     
-CREATE TABLE `user` (
-  `id` PRIMARY KEY INT NULL AUTO_INCREMENT,
-  `username` VARCHAR(32) NULL,
-  `password` MEDIUMTEXT NULL,
-  `first_name` MEDIUMTEXT NULL,
-  `last_name` MEDIUMTEXT NULL,
-  `email` MEDIUMTEXT NULL
+CREATE TABLE user (
+  id PRIMARY KEY INT NULL AUTO_INCREMENT,
+  username VARCHAR(32) NULL,
+  password MEDIUMTEXT NULL,
+  first_name MEDIUMTEXT NULL,
+  last_name MEDIUMTEXT NULL,
+  email MEDIUMTEXT NULL
 );
 
 -- ---
@@ -39,15 +39,15 @@ CREATE TABLE `user` (
 -- response to a request
 -- ---
 
-DROP TABLE IF EXISTS `submission`;
+DROP TABLE IF EXISTS submission;
     
-CREATE TABLE `submission` (
-  `id` PRIMARY KEY INT NULL AUTO_INCREMENT,
-  `request_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `submission_timestamp` TIMESTAMP NULL,
-  `content` BLOB NULL,
-  `filename` MEDIUMTEXT NOT NULL DEFAULT 'NULL',
+CREATE TABLE submission (
+  id PRIMARY KEY INT NULL AUTO_INCREMENT,
+  request_id INT NOT NULL,
+  user_id INT NOT NULL,
+  submission_timestamp TIMESTAMP NULL,
+  content BLOB NULL,
+  filename MEDIUMTEXT NOT NULL DEFAULT 'NULL',
   FOREIGN KEY(request_id) REFERENCES request(id),
   FOREIGN KEY(user_id) REFERENCES user(id)
 );
@@ -57,11 +57,11 @@ CREATE TABLE `submission` (
 -- tags are applied to a request to help search for it, allows for sorting as well
 -- ---
 
-DROP TABLE IF EXISTS `tag`;
+DROP TABLE IF EXISTS tag;
     
-CREATE TABLE `tag` (
-  `id` PRIMARY KEY INT NULL AUTO_INCREMENT,
-  `name` MEDIUMTEXT NULL
+CREATE TABLE tag (
+  id PRIMARY KEY INT NULL AUTO_INCREMENT,
+  name MEDIUMTEXT NULL
 );
 
 -- ---
@@ -69,13 +69,13 @@ CREATE TABLE `tag` (
 -- join table
 -- ---
 
-DROP TABLE IF EXISTS `request_tag`;
+DROP TABLE IF EXISTS request_tag;
     
-CREATE TABLE `request_tag` (
-  `id` INT NULL AUTO_INCREMENT,
-  `request_id` INT NOT NULL,
-  `tag_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `tag_id`, `request_id`),
+CREATE TABLE request_tag (
+  id INT NULL AUTO_INCREMENT,
+  request_id INT NOT NULL,
+  tag_id INT NOT NULL,
+  PRIMARY KEY (id, tag_id, request_id),
   FOREIGN KEY(request_id) REFERENCES request(id),
   FOREIGN KEY(tag_id) REFERENCES tag(id)
 );
