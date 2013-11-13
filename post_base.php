5b968513d8f55430f065401b0706915e11c4ff35
ArnoldClarke <aclarke@mines.edu>
2013-11-12 20:40:22 -0700

    Add base for request posts, ready for comment section

---
+++ post_base.php
@@ -0,0 +1,25 @@
+<?php include "base.php" ?>
+
+<!-- Title block --> 
+<?php startblock('title') ?>
+	Post View
+<?php endblock() ?>
+
+<?php startblock('content') ?>
+	
+	//Here be the title of the post
+	<h3 id ='title-request'> </h3>
+
+	//Here be the language of the post
+	<div id='language-request'><p> </p></div>
+	
+	
+	//Here be the description of the post
+	<div id = 'description-request'> <p> </p></div>
+	
+	
+	//Here be the list of replies
+	
+	//Here be comments
+	
+<?php endblock() ?>
