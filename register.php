e1955e9e11c00c87fc5a0e1f130383b0e37ecc53
Andrew C. Cook <andcook@luna.mines.edu>
2013-11-12 19:04:18 -0700

    Register form

--- register.php
+++ register.php
@@ -4,3 +4,25 @@
 <?php startblock('title') ?>
 	Register
 <?php endblock() ?>
+
+
+<?php startblock('content') ?>
+	<h3>Fill out the form below to create an account on PaidSource:</h3>
+	<form action="" method="POST">
+		<table>
+			<tr>
+				<td>Enter a username:</td>
+				<td><input type="text" name="username" /></td>
+			</tr>
+			<tr>
+				<td>Enter a password:</td>
+				<td><input type="password" name="password" /></td>
+			</tr>
+			<tr>
+				<td>Verify password:</td>
+				<td><input type="password" name="password2" /></td>
+			</tr>	
+		</table>
+		<button action="submit">Register</button>
+	</form>
+<?php endblock() ?>
