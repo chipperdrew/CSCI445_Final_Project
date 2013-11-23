893d9a8b1e4d9adae64c4042fccc4720c4f5bae7
Vander Rice <vanxrice@gmail.com>
2013-11-23 14:05:10 -0700

    Updated TODOs

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
 	Team 6
 	Due Date: 12/03/13
 	TODO:
-		- Check the login validity
-		- Save request with correct user
-		- Add Disqus for commenting
+		- User pages
+			- My Requests
+				- View Submissions
+				- Select Submission and/or Close Request
+			- My Submissions
 		- Styling
-		- Maintain user session
 -->
 <?php include 'base.php' ?>
 
