6d0c45eea88dda03d08a8eb42e59b186d638a387
ArnoldClarke <aclarke@mines.edu>
2013-12-02 20:38:39 -0700

    homepage descriptions

--- index.php
+++ index.php
@@ -28,10 +28,6 @@
 		echo '<div class="page-header">';
 		echo "<h2>Welcome to PaidSource!";
 
-		
-		echo '<div class ="well">';
-		echo "TODO: show requests";
-		echo "</div>";
 		echo "</div>";
 		
 		} else {
@@ -54,13 +50,38 @@
 			</table>
 			<button class="btn btn-success" action="submit">Login</button>
 		</form>';
+				
 		echo "</div>";
+		
+		
 	}
 ?>
 
 
 		<!-- //Brief Overview -->
-		
+		<div class="well">
+			<h2>Brief Introduction</h2>
+			<p>Hello and welcome to <strong>PaidSource. Here you can take advantage of the advanced communication systems
+			our time(the internet) to get help with your personal coding projects</p>
+			<p>How is this possible? Its quite simple actually. Here at <strong>PaidSource</strong> you can request code packets from other users
+			will help you to complete your projects(at a price). Likewise you can complete coding requests made by other users and get paid in the process!</p>
+			<p>A suggested first step is for you to finish reading this tutorial and either browse requests made by other users or register as a user and
+			join in the fun!</p>
+		</div>
+
+
+		<!-- How to browse requests -->
+		<div class ="well">
+			<h2>Browsing Requests</h2>
+			<p>To start browsing requests you can click the <strong>Code Requests</strong> tab at the top of this page</p>
+			<p>Once you have open the Code Requests page you are able to either add your own requests or browse requests
+			made by other users this browsing page will show you a list of requests made with the title of the request
+			and the name of the users making the request</p>
+			<p>To see a description of a specific request simple click on its Title. This will load a new page showing you the request's description
+			and possible payout for completing a working solution</p>
+			<p>Once you are a user with <strong>PaidSource</strong> you can also submit solutions from this page</p>
+		</div>
+
 		
 		<!-- //How to add a request -->
 
@@ -69,7 +90,6 @@
 		<!-- How to look at your own requests -->
 		
 		
-		<!-- How to browse requests -->
 		
 		<!-- When you are finished click the logout button and get money get paid -->
 
